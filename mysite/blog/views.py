99d62d04b0848235dd0bf4afc9f5440809c121d8
Jayson Scruggs <jayson.scruggs.work@gmail.com>
2022-05-29 11:20:10 -0500

    Added posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from .models import Post
 
-# Create your views here.
+class PostView(View):
+
+    def get(self, request, *args, **kwargs):
+        qs = Post.objects.values('title', 'content', 'author', 'date_posted')
+        data = {'posts': list(qs)}
+        return JsonResponse(data)
