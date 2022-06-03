aeff7cdfc6752ec4874ee29ed1cd7782862fe4d2
Jayson Scruggs <jayson.scruggs.work@gmail.com>
2022-06-02 21:54:09 -0500

    add update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 from http import HTTPStatus
 
@@ -15,20 +16,40 @@ class PostView(View):
 
     def get(self, request, *args, id_=None, **kwargs):
         if not id_:
-            qs = Post.objects.values('title', 'content', 'author', 'date_posted')
+            qs = Post.objects.order_by('date_posted').values('title', 'content', 'author', 'date_posted', 'last_updated')
         else:
-            qs = Post.objects.filter(pk=id_).values('title', 'content', 'author', 'date_posted')
+            qs = Post.objects.filter(pk=id_).values('title', 'content', 'author', 'date_posted', 'last_updated')
         data = {'posts': list(qs)}
         return JsonResponse(data)
 
     def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            try:
-                Post(**json.loads(request.body), author=request.user).save()
-                return JsonResponse({'message': 'Post saved successfully'}, status=HTTPStatus.CREATED)
-            except IntegrityError:
-                return JsonResponse({'message': 'A post with that title already exists!'}, status=HTTPStatus.BAD_REQUEST)
-            except Exception as e:
-                return JsonResponse({'message':'Invalid Request Data'}, status=HTTPStatus.BAD_REQUEST)
-        else:
+        if not request.user.is_authenticated:
             return JsonResponse({'message': 'Login to post'}, status=HTTPStatus.BAD_REQUEST)
+        try:
+            Post(**json.loads(request.body), author=request.user).save()
+            return JsonResponse({'message': 'Post saved successfully'}, status=HTTPStatus.CREATED)
+        except IntegrityError:
+            return JsonResponse({'message': 'A post with that title already exists!'}, status=HTTPStatus.BAD_REQUEST)
+        except Exception as e:
+            return JsonResponse({'message':'Invalid Request Data'}, status=HTTPStatus.BAD_REQUEST)
+            
+
+
+    def put(self, request, *args, id_, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({'message': 'Login to update a post'}, status=HTTPStatus.BAD_REQUEST)
+        post = Post.objects.filter(pk=id_).first()
+        try:
+            if not post.author == request.user:
+                return JsonResponse({'message': 'Only the author can edit a post!'}, status=HTTPStatus.UNAUTHORIZED)
+        except TypeError:
+            return JsonResponse({'message': 'Post not found'}, status=HTTPStatus.BAD_REQUEST)
+        data = json.loads(request.body)
+        post.title = data.get('title',post.title)
+        post.content = data.get('content', post.content)
+        post.last_updated = datetime.now()
+        post.save()
+        return JsonResponse({'message': 'Updated'}, status=HTTPStatus.ACCEPTED)
+
+    def delete(self, request, *args, id_, **kwargs):
+        pass # TODO
